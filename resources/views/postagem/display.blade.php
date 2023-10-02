cfb3cc553d216d6298f29fc2523db0d8f37c9ab7
Anna Júlia Lauton <annajulia.costa15@gmail.com>
2023-10-02 11:02:44 -0300

    Alteração visualizar Postagem

--- resources/views/postagem/display.blade.php
+++ resources/views/postagem/display.blade.php
@@ -15,9 +15,7 @@
     }
 @endphp
 
-
 <div id="demo" class="container carousel slide" data-bs-ride="carousel" data-bs-interval="6000">
-
     <div class="carousel-indicators" id="carousel-indicators">
         <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
         @foreach ($imagensPostagens as $index => $img)
@@ -54,12 +52,10 @@
             @foreach($postagens as $postagem)
             <div class="col">
                 <div class="card shadow-sm">
-
                     @if (count($postagem->imagens) > 0)
                     @foreach ($postagem->imagens as $img)
                     @if (Storage::disk('public')->exists($img->imagem))
                     <img src="{{ URL::asset('storage') }}/{{ $img->imagem }}" alt="{{ $postagem->titulo }}" class="bd-placeholder-img card-img-top" width="100%">
-
                     @endif
                     @endforeach
                     @endif
@@ -68,7 +64,7 @@
                         <p class="card-text">{{ $postagem->titulo }}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="btn-group">
-                                <a href="#" class="btn btn-sm btn-outline-secondary">Visualizar</a>
+                                <a href="{{ route('postagem.show', ['id' => $postagem->id]) }}" class="btn btn-sm btn-outline-secondary">Visualizar</a>
                             </div>
                             <small class="text-body-secondary">9 mins</small>
                         </div>
