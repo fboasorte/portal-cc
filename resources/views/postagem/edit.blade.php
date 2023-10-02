03d2807986e8dea9a15b24daa0578ff693ed121e
Anna Júlia Lauton <annajulia.costa15@gmail.com>
2023-10-02 12:01:49 -0300

    Atualização tela editar postagem

--- resources/views/postagem/edit.blade.php
+++ resources/views/postagem/edit.blade.php
@@ -6,8 +6,8 @@
 <div class="custom-container">
     <div>
         <div>
-            <i class="fas fa-paste fa-2x"></i>
-            <h3 class="smaller-font">Gerenciar Postagem</h3>
+            <i class="fas fa-pen-to-square fa-2x"></i>
+            <h3 class="smaller-font" class="form-label">Editar Postagem</h3>
         </div>
     </div>
 </div>
@@ -16,73 +16,69 @@
         @csrf
         @method('PUT')
         <div class="form-group">
-            <label for="titulo">Título</label>
-            <input type="text" value="{{ $postagem->titulo }}" name="titulo" id="titulo" class="form-control"
-                placeholder="Título da postagem" required>
+            <label for="titulo" class="form-label"><br>Título:</label>
+            <input type="text" value="{{ $postagem->titulo }}" name="titulo" id="titulo" class="form-control" placeholder="Título da postagem" required>
         </div>
         <div class="form-group">
-            <label for="texto">Texto</label>
+            <label for="texto" class="form-label">Texto:</label>
             <textarea name="texto" id="texto" class="form-control" placeholder="Texto da postagem" required>{{ $postagem->texto }}</textarea>
         </div>
         <div class="form-group">
-            <label for="tipo_postagem">Tipo</label>
+            <label for="tipo_postagem" class="form-label">Tipo:</label>
             <select name="tipo_postagem_id" id="tipo_postagem_id" class="form-control" required>
                 @foreach ($tipo_postagens as $key => $value)
-                    <option value="{{ $key }}" {{ $key == $postagem->tipo_postagem_id ? 'selected' : '' }}>
-                        {{ $value }}
-                    </option>
+                <option value="{{ $key }}" {{ $key == $postagem->tipo_postagem_id ? 'selected' : '' }}>
+                    {{ $value }}
+                </option>
                 @endforeach
             </select>
         </div>
         <div class="form-group">
-            <label for="tipo_postagem">Exibe no menu inicial?</label>
+            <label for="tipo_postagem" class="form-label">Exibe no menu inicial?</label>
             <input type="checkbox" name="menu_inicial" id="menu_inicial" {{$postagem->menu_inicial ? 'checked' : ''}}>
         </div>
         <div class="form-group">
-            <label for="imagem">Imagem</label>
+            <label for="imagem" class="form-label">Imagens (2000 x 378):</label>
             @if (count($postagem->imagens) > 0)
-                @foreach ($postagem->imagens as $img)
-                    <button class="btn text-danger" type="submit" form="deletar-imagens{{ $img->id }}">X</button>
-                    <img src="{{ URL::asset('storage') }}/{{ $img->imagem }}" class="img-responsive"
-                        style="max-height:100px; max-width:100px;">
-                @endforeach
+            @foreach ($postagem->imagens as $img)
+            <button class="btn text-danger" type="submit" form="deletar-imagens{{ $img->id }}">X</button>
+            <img src="{{ URL::asset('storage') }}/{{ $img->imagem }}" class="img-responsive" style="max-height:100px; max-width:100px;">
+            @endforeach
             @endif
             <input type="file" name="imagens[]" id="imagens" class="form-control" multiple>
         </div>
 
         <div class="form-group">
-            <label for="arquivo">Arquivo</label>
+            <label for="arquivo" class="form-label">Arquivos:</label>
             @if (count($postagem->arquivos) > 0)
-                @foreach ($postagem->arquivos as $arquivo)
-                    <button class="btn text-danger" type="submit" form="deletar-arquivos{{ $arquivo->id }}">X</button>
-                    <a href="{{ URL::asset('storage') }}/{{ $arquivo->path }}">{{ $arquivo->nome }}</a>
-                @endforeach
+            @foreach ($postagem->arquivos as $arquivo)
+            <button class="btn text-danger" type="submit" form="deletar-arquivos{{ $arquivo->id }}">X</button>
+            <a href="{{ URL::asset('storage') }}/{{ $arquivo->path }}">{{ $arquivo->nome }}</a>
+            @endforeach
             @endif
             <input type="file" name="arquivos[]" id="arquivos" class="form-control" multiple>
         </div>
 
         <button type="submit" class="btn custom-button btn-default">Salvar</button>
-        <button class="btn custom-button custom-button-castastrar-tcc btn-default"><a href="{{route('postagem.index')}} "class= "btn-back" >Cancelar</a></button>
+        <button class="btn custom-button custom-button-castastrar-tcc btn-default"><a href="{{route('postagem.index')}} " class="btn-back">Cancelar</a></button>
     </form>
 
     @if (count($postagem->imagens) > 0)
-        @foreach ($postagem->imagens as $img)
-            <form id="deletar-imagens{{ $img->id }}"
-                action="{{ route('postagem.delete_imagem', ['id' => $img->id]) }}" method="post">
-                @csrf
-                @method('delete')
-            </form>
-        @endforeach
+    @foreach ($postagem->imagens as $img)
+    <form id="deletar-imagens{{ $img->id }}" action="{{ route('postagem.delete_imagem', ['id' => $img->id]) }}" method="post">
+        @csrf
+        @method('delete')
+    </form>
+    @endforeach
     @endif
 
     @if (count($postagem->arquivos) > 0)
-        @foreach ($postagem->arquivos as $arquivo)
-            <form id="deletar-arquivos{{ $arquivo->id }}"
-                action="{{ route('postagem.delete_arquivo', ['id' => $arquivo->id]) }}" method="post">
-                @csrf
-                @method('delete')
-            </form>
-        @endforeach
+    @foreach ($postagem->arquivos as $arquivo)
+    <form id="deletar-arquivos{{ $arquivo->id }}" action="{{ route('postagem.delete_arquivo', ['id' => $arquivo->id]) }}" method="post">
+        @csrf
+        @method('delete')
+    </form>
+    @endforeach
     @endif
 </div>
 @stop
