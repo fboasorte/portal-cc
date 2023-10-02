a1a67c9ebff1cf18a74b9f2801769a418f77b23e
yodemisj <yodemisj@gmail.com>
2023-10-01 21:41:56 -0300

    Atualizações nas views create e edit de curso!

--- resources/views/curso/create.blade.php
+++ resources/views/curso/create.blade.php
@@ -23,31 +23,31 @@
 
         <form method="post" action="{{ route('curso.store') }}">
             @csrf
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="turno">Nome:</label>
-                <input type="text" id="nome" name="nome" placeholder="Informe o nome do Curso" required>
+                <input class="form-control" type="text" id="nome" name="nome" placeholder="Informe o nome do Curso" required>
             </div>
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="turno">Turno:</label>
 
-                <select id="turno" name="turno" required>
+                <select class="form-control" id="turno" name="turno" required >
                     <option value="Matutino">Matutino</option>
                     <option value="Vespertino">Vespertino</option>
                     <option value="Noturno">Noturno</option>
                     <option value="Integral">Integral</option>
                 </select>
             </div>
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="carga-horaria">Carga Horária:</label>
-                <input type="Number" id="carga_horaria" name="carga_horaria" placeholder="Informe a carga horária">
+                <input class="form-control" type="Number" id="carga_horaria" name="carga_horaria" placeholder="Informe a carga horária">
             </div>
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="sigla">Sigla:</label>
-                <input type="text" id="sigla" name="sigla" placeholder="Informe a sigla"  maxlength="5">
+                <input class="form-control" type="text" id="sigla" name="sigla" placeholder="Informe a sigla"  maxlength="5">
             </div>
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="analytics">Analytics:</label>
-                <input type="text" id="analytics" name="analytics" placeholder="Informe o analytics">
+                <input class="form-control" type="text" id="analytics" name="analytics" placeholder="Informe o analytics">
             </div>
             <button type="submit" class="btn custom-button btn-default">Cadastrar</button>
             <button class="btn custom-button custom-button-castastrar-tcc btn-default"><a
--- resources/views/curso/edit.blade.php
+++ resources/views/curso/edit.blade.php
@@ -24,30 +24,30 @@
             @csrf
             @method('PUT') {{-- Usamos @method('PUT') para indicar que esta é uma atualização --}}
 
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="nome">Nome:</label>
-                <input type="text" id="nome" name="nome" value="{{ $curso->nome }}" placeholder="Informe o nome do Curso" required>
+                <input class="form-control" type="text" id="nome" name="nome" value="{{ $curso->nome }}" placeholder="Informe o nome do Curso" required>
             </div>
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="turno">Turno:</label>
-                <select id="turno" name="turno" required>
+                <select class="form-control" id="turno" name="turno" required>
                     <option value="Matutino" @if($curso->turno == 'Matutino') selected @endif>Matutino</option>
                     <option value="Vespertino" @if($curso->turno == 'Vespertino') selected @endif>Vespertino</option>
                     <option value="Noturno" @if($curso->turno == 'Noturno') selected @endif>Noturno</option>
                     <option value="Integral" @if($curso->turno == 'Integral') selected @endif>Integral</option>
                 </select>
             </div>
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="carga_horaria">Carga Horária:</label>
-                <input type="number" id="carga_horaria" name="carga_horaria" value="{{ $curso->carga_horaria }}" placeholder="Informe a carga horária">
+                <input class="form-control" type="number" id="carga_horaria" name="carga_horaria" value="{{ $curso->carga_horaria }}" placeholder="Informe a carga horária">
             </div>
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="sigla">Sigla:</label>
-                <input type="text" id="sigla" name="sigla" value="{{ $curso->sigla }}" placeholder="Informe a sigla" maxlength="5">
+                <input class="form-control" type="text" id="sigla" name="sigla" value="{{ $curso->sigla }}" placeholder="Informe a sigla" maxlength="5">
             </div>
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="analytics">Analytics:</label>
-                <input type="text" id="analytics" name="analytics" value="{{ $curso->analytics }}" placeholder="Informe o analytics">
+                <input class="form-control" type="text" id="analytics" name="analytics" value="{{ $curso->analytics }}" placeholder="Informe o analytics">
             </div>
             <button type="submit" class="btn custom-button btn-default">Atualizar</button>
             <a href="{{ route('curso.index') }}" class="btn custom-button custom-button-castastrar-tcc btn-default btn-back">Cancelar</a>
