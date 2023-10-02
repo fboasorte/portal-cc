a4d74bc3f61018214d35e4b153d333f1f483501e
Anna Júlia Lauton <annajulia.costa15@gmail.com>
2023-10-02 15:42:12 -0300

    Atualização busca e icons de Gerenciar Professor

--- resources/views/professor/index.blade.php
+++ resources/views/professor/index.blade.php
@@ -3,68 +3,77 @@
 @section('title', 'Professores')
 
 @section('content')
-    <div class="custom-container">
+<div class="custom-container">
+    <div>
         <div>
-            <div>
-                <i class="fas fa-person-chalkboard fa-2x"></i>
-                <h3 class="smaller-font">Gerenciar Professor</h3>
-            </div>
+            <i class="fas fa-person-chalkboard fa-2x"></i>
+            <h3 class="smaller-font">Gerenciar Professor</h3>
         </div>
     </div>
-    <div class="container">
-        <div class="row campo-busca">
-            <div class="col-md-12">
-                <input type="text" id="searchInput" class="form-control field-search" placeholder="Buscar em todos os campos"
-                    aria-label="Buscar">
-            </div>
+</div>
+<div class="container">
+    <div class="row campo-busca">
+        <div class="col-md-12">
+            <input type="text" id="searchInput" class="form-control field-search" placeholder="Buscar em todos os campos" aria-label="Buscar">
         </div>
-        <div>
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header text-white div-form">
-                        Professores
-                        <a href="{{ route('professor.create') }}" class="btn btn-success btn-sm float-end">Cadastrar</a>
-                    </div>
-                    <div class="card-body">
+    </div>
+    <div>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header text-white div-form">
+                    Professores
+                    <a href="{{ route('professor.create') }}" class="btn btn-success btn-sm float-end">Cadastrar</a>
+                </div>
+                <div class="card-body">
 
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nome</th>
-                                    <th>Email</th>
-                                    <th>Criação</th>
-                                    <th>Ação</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($servidores as $servidor)
-                                    <tr>
-                                        <td>{{ $servidor->id }}</td>
-                                        <td>{{ $servidor->nome }}</td>
-                                        <td>{{ $servidor->email }}</td>
-                                        <td>{{ date_format($servidor->created_at, 'd/m/Y H:i:s') }}</td>
-                                        <td>
-                                            <form method="POST" action="{{ route('professor.destroy', $servidor->id) }}">
-                                                @csrf
-                                                <input name="_method" type="hidden" value="DELETE">
-                                                <a class="btn btn-success btn-sm"
-                                                    href="{{ route('professor.show', $servidor->id) }}">Visualizar</a>
-                                                <a class="btn btn-primary btn-sm"
-                                                    href="{{ route('professor.edit', $servidor->id) }}">Editar</a>
-                                                <button type="submit" class="btn btn-danger btn-sm" title='Delete'
-                                                    onclick="return confirm('Deseja realmente excluir esse registro?')"><i class="fas fa-trash"></i></button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <table id="professorTable" class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>Criação</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($servidores as $servidor)
+                            <tr>
+                                <td>{{ $servidor->id }}</td>
+                                <td>{{ $servidor->nome }}</td>
+                                <td>{{ $servidor->email }}</td>
+                                <td>{{ date_format($servidor->created_at, 'd/m/Y H:i:s') }}</td>
+                                <td>
+                                    <form method="POST" action="{{ route('professor.destroy', $servidor->id) }}">
+                                        @csrf
+                                        <input name="_method" type="hidden" value="DELETE">
+                                        <a class="btn btn-success btn-sm" href="{{ route('professor.show', $servidor->id) }}"><i class="fa-solid fa-eye"></i></a>
+                                        <a class="btn btn-primary btn-sm" href="{{ route('professor.edit', $servidor->id) }}"><i class="fas fa-pencil-alt"></i></a>
+                                        <button type="submit" class="btn btn-danger btn-sm" title='Delete' onclick="return confirm('Deseja realmente excluir esse registro?')"><i class="fas fa-trash"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
 
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
+</div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        $("#searchInput").on("keyup", function() {
+            var searchText = $(this).val().toLowerCase();
+            $("#professorTable tbody tr").filter(function() {
+                // Excluindo a última coluna que é a de ação do filtro
+                var rowData = $(this).find("td:not(:last-child)").text().toLowerCase();
+                $(this).toggle(rowData.indexOf(searchText) > -1);
+            });
+        });
+    });
+</script>
 @stop
