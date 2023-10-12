8db58d5645e772156b646976c30ea02b48e991cd
Anna Júlia Lauton <annajulia.costa15@gmail.com>
2023-10-12 10:24:00 -0300

    Atualização tela editar colegiado

--- resources/views/colegiado/edit.blade.php
+++ resources/views/colegiado/edit.blade.php
@@ -3,100 +3,97 @@
 @section('title', 'Editar Colegiado')
 
 @section('content')
-    <div class="custom-container">
+<div class="custom-container">
+    <div>
         <div>
-            <div>
-                <i class="fas fa-chalkboard fa-2x"></i>
-                <h3 class="smaller-font">Editar colegiado</h3>
-            </div>
+            <i class="fas fa-chalkboard fa-2x"></i>
+            <h3 class="smaller-font">Editar colegiado</h3>
         </div>
     </div>
-    <div class="container">
-        <form method="post" action="{{ route('colegiado.update', [$colegiado->id]) }}" enctype="multipart/form-data"
-            id="form-colegiado">
-            @method('PUT')
-            @csrf
+</div>
+<div class="container mt-4">
+    <form method="post" action="{{ route('colegiado.update', [$colegiado->id]) }}" enctype="multipart/form-data" id="form-colegiado">
+        @method('PUT')
+        @csrf
+        <div class="form-group">
             <div class="form-group">
-                <div class="form-group">
+                <div class="mb-3">
                     <h5>Portaria PDF</h5>
-                    <label for="arquivo">Arquivo</label>
+                    <label for="arquivo" class="form-label">Arquivo</label>
                     <input type="file" name="arquivo" id="arquivo" class="form-control">
                 </div>
+            </div>
 
-                <div>
-                    <label for="local">Número portaria*</label>
-                    <input type="number" name="numero_portaria" id="numero_portaria" class="form-control"
-                        value="{{ $colegiado->numero_portaria }}" required>
-                    <label for="vigencia">Vigência*</label>
-                    <input type="date" name="vigencia_inicio" id="vigencia_incio" class="form-control"
-                        value="{{ date('Y-m-d', strtotime($colegiado->inicio)) }}" required> até
+            <div>
+                <div class="mb-3">
+                    <label for="local" class="form-label">Número portaria*</label>
+                    <input type="number" name="numero_portaria" id="numero_portaria" class="form-control" value="{{ $colegiado->numero_portaria }}" required>
+                </div>
+                <div class="mb-3">
+                    <label for="vigencia" class="form-label">Vigência*</label>
+                    <input type="date" name="vigencia_inicio" id="vigencia_incio" class="form-control" value="{{ date('Y-m-d', strtotime($colegiado->inicio)) }}" required> até
 
-                        <input type="date" name="vigencia_fim" id="vigencia_fim" class="form-control"
-                        required min="{{ date('Y-m-d', strtotime($hoje . ' +1 day')) }}" required>
+                    <input type="date" name="vigencia_fim" id="vigencia_fim" class="form-control" required min="{{ date('Y-m-d', strtotime($hoje . ' +1 day')) }}" required>
                 </div>
             </div>
-            <div class="form-group">
-                <h5>Professores </h5>
+        </div>
+        <div class="form-group">
+            <h5>Professores </h5>
 
-                <div class="form-group" id="professores">
-                    <label for="professores">Selecione 4 professores: </label>
+            <div class="form-group" id="professores">
+                <div class="mb-3">
+                    <label for="professores" class="form-label">Selecione 4 professores: </label>
 
                     @foreach ($professores as $professor)
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" name="professores[]"
-                                id="professor_{{ $professor->id }}" value="{{ $professor->id }}"
-                                {{ $colegiado->professores->contains($professor->id) ? 'checked' : '' }}>
-                            <label for="professor_{{ $professor->id }}"
-                                class="form-check-label">{{ $professor->servidor->nome }}</label>
-                        </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" name="professores[]" id="professor_{{ $professor->id }}" value="{{ $professor->id }}" {{ $colegiado->professores->contains($professor->id) ? 'checked' : '' }}>
+                        <label for="professor_{{ $professor->id }}" class="form-check-label">{{ $professor->servidor->nome }}</label>
+                    </div>
                     @endforeach
                 </div>
-                <a href="" class="btn custom-button modal-trigger" data-bs-toggle="modal"
-                    data-bs-target="#createProfessor">Cadastrar novo professor</a>
+            </div>
+            <a href="" class="btn custom-button modal-trigger" data-bs-toggle="modal" data-bs-target="#createProfessor">Cadastrar novo professor</a>
 
-                <div class="form-group" id="alunos">
+
+            <div class="form-group" id="alunos">
+                
+                    <br>
                     <h5>Alunos</h5>
-                    <label for="">Selecione pelo menos 1 e no máximo 4 alunos: </label>
+                    <label for="" class="form-label">Selecione pelo menos 1 e no máximo 4 alunos: </label>
 
                     @foreach ($alunos as $aluno)
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" name="alunos[]" id="aluno_{{ $aluno->id }}"
-                                value="{{ $aluno->id }}"
-                                {{ $colegiado->alunos->contains($aluno->id) ? 'checked' : '' }}>
-                            <label for="aluno_{{ $aluno->id }}" class="form-check-label">{{ $aluno->nome }}</label>
-                        </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" name="alunos[]" id="aluno_{{ $aluno->id }}" value="{{ $aluno->id }}" {{ $colegiado->alunos->contains($aluno->id) ? 'checked' : '' }}>
+                        <label for="aluno_{{ $aluno->id }}" class="form-check-label">{{ $aluno->nome }}</label>
+                    </div>
                     @endforeach
-                </div>
-                <a href="" class="btn custom-button modal-trigger" data-bs-toggle="modal"
-                    data-bs-target="#createAluno">Cadastrar aluno</a>
-
-                <div class="form-group" id="servidores">
-                    <h5>Servidor</h5>
-                    <label for="">Selecione pelo menos 1 e no máximo 4 servidores para técnicos administrativos:
-                    </label>
-
-                    @foreach ($servidores as $servidor)
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" name="servidores[]"
-                                id="servidor_{{ $servidor->id }}" value="{{ $servidor->id }}"
-                                {{ $colegiado->tecnicosAdm->contains($servidor->id) ? 'checked' : '' }}>
-                            <label for="servidor_{{ $servidor->id }}"
-                                class="form-check-label">{{ $servidor->nome }}</label>
-                        </div>
-                    @endforeach
-
-                </div>
-                <a href="" class="btn custom-button modal-trigger" data-bs-toggle="modal"
-                    data-bs-target="#createServidor">Cadastrar servidor</a>
+               
             </div>
-            <button type="submit" class="btn custom-button custom-button-castastrar-tcc btn-default">Atualizar</button>
-            <a href="{{ route('colegiado.index') }} "
-                class="btn custom-button custom-button-castastrar-tcc btn-default">Cancelar</a>
-        </form>
-    </div>
-    @include('modal.createProfessor')
-    @include('modal.createAluno')
-    @include('modal.createServidor')
+            <a href="" class="btn custom-button modal-trigger" data-bs-toggle="modal" data-bs-target="#createAluno">Cadastrar aluno</a>
+
+            <div class="form-group" id="servidores">
+                <br>
+                <h5>Servidor</h5>
+                <label for="" class="form-label">Selecione pelo menos 1 e no máximo 4 servidores para técnicos administrativos:
+                </label>
+
+                @foreach ($servidores as $servidor)
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" name="servidores[]" id="servidor_{{ $servidor->id }}" value="{{ $servidor->id }}" {{ $colegiado->tecnicosAdm->contains($servidor->id) ? 'checked' : '' }}>
+                    <label for="servidor_{{ $servidor->id }}" class="form-check-label">{{ $servidor->nome }}</label>
+                </div>
+                @endforeach
+
+            </div>
+            <a href="" class="btn custom-button modal-trigger" data-bs-toggle="modal" data-bs-target="#createServidor">Cadastrar servidor</a>
+        </div>
+        <button type="submit" class="btn custom-button custom-button-castastrar-tcc btn-default">Atualizar</button>
+        <a href="{{ route('colegiado.index') }} " class="btn custom-button custom-button-castastrar-tcc btn-default">Cancelar</a>
+    </form>
+</div>
+@include('modal.createProfessor')
+@include('modal.createAluno')
+@include('modal.createServidor')
 @stop
 <script>
     document.addEventListener("DOMContentLoaded", function() {
