cde70a138297fe7bd109552ee7188c17243c1de4
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2023-10-26 22:14:11 -0300

    Update show.blade.php
    atualiza página projetos, visão do usuário

--- resources/views/projeto/show.blade.php
+++ resources/views/projeto/show.blade.php
@@ -5,6 +5,79 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+
+<style>
+
+.bloco1{
+  background-color:#1c2c4c;
+  display:flex;
+  flex-direction:row;
+  flex-wrap:nowrap;
+  align-items:stretch;
+  justify-content:space-between;
+  min-width:1000px;
+
+
+  border:1px solid black;
+  border-radius:30px;
+  overflow:hidden;
+  margin:40px 13px;
+}
+
+.bloco3{
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  width:600px;
+
+  border-radius:30px;
+
+
+}
+
+
+.bloco4{
+
+  display:flex;
+  flex-direction:row;
+  justify-content:space-between;
+  align-items:center;
+
+
+  width:500%;
+  max-width:500px;
+  color:white;
+  text-align:center;
+  font-size:110%;
+
+}
+
+.bloco41{
+  padding: 0.6% 0.1%;
+  text-align:center;
+
+  width:50%;
+}
+
+
+
+
+.texto{
+
+  display: table-cell;
+  vertical-align: bottom;
+  background-color:white;
+  font-size:150%;
+  font-weight:bold;
+  text-align:center;
+  text-shadow: 0.1px 0.1px;
+
+ -moz-border-radius: 10px 10px 0 0;
+
+}
+
+</style>
+
 @php
     $meses = [
         1 => 'Janeiro',
@@ -30,61 +103,77 @@
     </div>
 </div>
 <br>
-<div class="event-schedule-area-two bg-color pad100">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade active show" id="home" role="tabpanel">
-                        <div class="table-responsive">
-                            <table id="projetoTable" class="table custom-table table-hover">
-                                <thead class="custom-table-head">
-                                    <tr>
-                                        <th class="text-center" scope="col">Data de Inicio</th>
-                                        <th class="text-center" scope="col">Data de Encerramento</th>
-                                        <th class="text-center" scope="col">Descrição</th>
-                                        <th class="text-center" scope="col">Professor</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($projetos as $projeto)
-                                    <tr class="inner-box clickable-row linha" data-href="{{ route('projeto.view', ['id' => $projeto->id]) }}">
-                                        <th scope="row">
-                                            <div class="event-date">
-                                                <span class="date-day">{{ \Carbon\Carbon::parse($projeto->data_inicio)->format('d') }}</span><br>
-                                                <span class="date-month">{{ $meses[\Carbon\Carbon::parse($projeto->data_inicio)->month] }}</span><br>
-                                                <span class="date-year">{{ \Carbon\Carbon::parse($projeto->data_inicio)->format('Y') }}</span>
-                                            </div>
-                                        </th>
-                                        <th>
-                                            <div class="event-date">
-                                                <span class="date-day">{{ \Carbon\Carbon::parse($projeto->data_termino)->format('d') }}</span><br>
-                                                <span class="date-month">{{ $meses[\Carbon\Carbon::parse($projeto->data_termino)->month] }}</span><br>
-                                                <span class="date-year">{{ \Carbon\Carbon::parse($projeto->data_termino)->format('Y') }}</span>
-                                            </div>
-                                        </th>
-                                        <td>
-                                            <div class="event-student text-center">
-                                                <span>{{ $projeto->descricao}}</span>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="event-orientador text-center">
-                                                <span>{{ $projeto->nome_professor }}</span>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+
+  <div style = "text-align:center; font-weight:bold; font-size:170%; margin-top:20px; padding: 8px 0px; border-top:0.4px solid gray; border-bottom: 0.4px solid gray;">
+    <span>Lista de projetos</span>
+  </div>
+
+  <div>
+    @forelse ($projetos as $projeto)
+    <div class="inner-box clickable-row linha bloco1" data-href="{{ route('projeto.view', ['id' => $projeto->id]) }}">
+
+      <div class = "bloco3">
+        <div class = "bloco4" style = "border-bottom:1px solid gray;">
+
+          <div class = "borda3 bloco41">
+              <div class="text-center" scope="col">Data de Inicio: </div>
+          </div>
+
+
+          <div class = "bloco41" >
+              <div class="text-center" style = "color:white">
+                  <span class="date-day">{{ \Carbon\Carbon::parse($projeto->data_inicio)->format('d') }}</span><br>
+                  <span class="date-month">{{ $meses[\Carbon\Carbon::parse($projeto->data_inicio)->month] }}</span><br>
+                  <span class="date-year">{{ \Carbon\Carbon::parse($projeto->data_inicio)->format('Y') }}</span>
+              </div>
+          </div>
+        </div>
+
+        <div class = "bloco4">
+
+          <div class = "borda3 bloco41" >
+              <div class="text-center" scope="col">Data de Encerramento:</div>
+          </div>
+
+          <div class = "bloco41" >
+              <div class="text-center" style = "color:white;">
+                  <span class="date-day">{{ \Carbon\Carbon::parse($projeto->data_termino)->format('d') }}</span><br>
+                  <span class="date-month">{{ $meses[\Carbon\Carbon::parse($projeto->data_termino)->month] }}</span><br>
+                  <span class="date-year">{{ \Carbon\Carbon::parse($projeto->data_termino)->format('Y') }}</span>
+              </div>
+          </div>
+        </div>
+
+    </div>
+    <div class = "bloco3" style = "background-color:white; border-radius:0px; width:60%; ">
+      <div class = "texto">
+        <span>Projeto criado por {{ $projeto->nome_professor }} .</span>
+        <br>
+        <span>Clique para ver detalhes sobre o projeto</span>
+      </div>
+    </div>
+
+        <!--
+        <div style = "background-color:red;">
+            <div class=" event-student text-center">
+                <span>{{ $projeto->descricao}}</span>
             </div>
         </div>
-    </div>
-</div>
 
+        <div style = "background-color:purple;">
+            <div class=" event-orientador text-center">
+                <span>{{ $projeto->nome_professor }}</span>
+            </div>
+        </div>
+      -->
+  </div>
+  @empty
+  <div style = "text-align:center; font-size:150%;">
+    <span>Nenhum projeto foi criado.</span>
+  </div>
+  @endforelse
+
+</div>
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
