85c764bdf5ef0115b4c3d91609833c70601c01b8
fboasorte <feliperocha.boasorte@gmail.com>
2023-10-30 20:33:38 -0300

    Conserta regras para projeto

--- app/Http/Requests/ProjetoRequest.php
+++ app/Http/Requests/ProjetoRequest.php
@@ -22,15 +22,15 @@ class ProjetoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'titulo' => ['required', 'string'],
-            'descricao' => ['required', 'string'],
-            'resultados' => ['nullable', 'string'],
+            'titulo' => ['required', 'string',  'max:65535'],
+            'descricao' => ['required', 'string', 'max:65535'],
+            'resultados' => ['nullable', 'string',  'max:65535'],
             'data_inicio' => ['required', 'date', 'after:01/01/2013'],
             'data_termino' => ['nullable', 'date', 'after:data_inicio', 'before:+10 years'],
-            'palavras_chave' => ['required', 'string'],
+            'palavras_chave' => ['required', 'string',  'max:65535'],
             'professor_id' => ['required', 'integer'],
-            'fomento' => ['nullable', 'string'],
-            'link' => ['nullable', 'url'],
+            'fomento' => ['nullable', 'string',  'max:255'],
+            'link' => ['nullable', 'url', 'max:255'],
             'imagens.*' => ['image']
         ];
     }
@@ -38,19 +38,45 @@ class ProjetoRequest extends FormRequest
     public function messages()
     {
         return [
-            'titulo.required' => 'O título é obrigatório',
-            'descricao.required' => 'A descrição é obrigatória',
-            'resultados.string' => 'Os resultados devem conter texto',
-            'data_inicio.required' => 'A data de início é obrigatória',
-            'data_inicio.date' => 'A data de início deve ser uma data válida',
-            'data_inicio.after' => 'A data de início deve ser dos últimos 10 anos',
-            'data_termino.date' => 'A data de término deve ser uma data válida',
-            'data_termino.after' => 'A data de término não pode ser antes da data de início',
-            'data_termino.before' => 'A data de término não pode passar dos próximos 10 anos',
-            'palavras_chave.required' => 'As palavras chave são obrigatórias',
-            'professor_id.required' => 'Um professor responsável é obrigatório',
-            'link.url' => 'O link deve conter uma URL válida',
-            'imagens.*.image' => 'Imagens deve conter apenas imagens'
+            'titulo' => [
+                'required' => 'O título é obrigatório',
+                'max' => 'O título pode ter no máximo 65535 caracteres',
+            ],
+            'descricao' => [
+                'required' => 'A descrição é obrigatória',
+                'max' => 'A descrição pode ter no máximo 65535 caracteres',
+            ],
+            'resultados' => [
+                'string' => 'Os resultados devem conter texto',
+                'max' => 'Os resultados podem ter no máximo 65535 caracteres',
+            ],
+            'data_inicio' => [
+                'required' => 'A data de início é obrigatória',
+                'date' => 'A data de início deve ser uma data válida',
+                'after' => 'A data de início deve ser dos últimos 10 anos',
+            ],
+            'data_termino' => [
+                'date' => 'A data de término deve ser uma data válida',
+                'after' => 'A data de término não pode ser antes da data de início',
+                'before' => 'A data de término não pode passar dos próximos 10 anos',
+            ],
+            'palavras_chave' => [
+                'palavras_chave.required' => 'As palavras chave são obrigatórias',
+                'palavras_chave.max' => 'As palavras chaves devem conter no máximo 65535 caracteres',
+            ],
+            'professor_id' => [
+                'required' => 'Um professor responsável é obrigatório',
+            ],
+            'fomento' => [
+                'max' => 'O fomento deve ter no máximo 255 caracteres',
+            ],
+            'link' => [
+                'url' => 'O link deve conter uma URL válida',
+                'max' => 'O link deve ter no máximo 255 caracteres',
+            ],
+            'imagens.*' => [
+                'image' => 'Apenas imagens podem ser enviadas'
+            ],
         ];
     }
 }
