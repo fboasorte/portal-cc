722fa221b5de3809e8953b97fc43c3c8a45568ac
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2023-10-31 11:50:52 -0300

    corrige cor concluído

--- resources/views/projeto/show.blade.php
+++ resources/views/projeto/show.blade.php
@@ -115,9 +115,9 @@
                                         <a href="{{ route('projeto.view', ['id' => $projeto->id]) }}">{{$projeto->titulo}}</a>
 
                                         @if( date("Y-m-d") > $projeto->data_termino)
-                                            <span class="badge bg-warning">Concluído</span>
+                                            <span class="badge bg-success">Concluído</span>
                                         @else
-                                            <span class="badge bg-success">Não concluído</span>
+                                            <span class="badge bg-warning">Não concluído</span>
                                         @endif
 
                                     </li>
@@ -180,9 +180,9 @@
                                         <a href="{{ route('projeto.view', ['id' => $projeto->id]) }}">{{$projeto->titulo}}</a>
                                         
                                         @if( date("Y-m-d") > $projeto->data_termino)
-                                            <span class="badge bg-warning">Concluído</span>
+                                            <span class="badge bg-success" >Concluído</span>
                                         @else
-                                            <span class="badge bg-success">Não concluído</span>
+                                            <span class="badge bg-warning">Não concluído</span>
                                         @endif
 
                                     </li>
